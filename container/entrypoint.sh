e535bb7f9f27552b453e5b4b0ff88fdd55737dd7
Giles Knap <giles.knap@diamond.ac.uk>
2025-08-05 07:20:58 +0100

    get launch working fully

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-DIR=/tmp/example-services$$
+DIR=/tmp/example-services
 mkdir -p "$DIR"
 
 rsync -a --exclude=.git --exclude=container "/workspace/" "$DIR"
 
 cd "$DIR"
-docker compose up -d
+docker compose up
+
--- container/example-launch.sh
+++ container/example-launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-podman run -it -v /tmp:/tmp -v $XDG_RUNTIME_DIR/podman/podman.sock:/var/run/docker.sock example-services:latest
+podman run --rm -it --security-opt label=disable -e DISPLAY -v /tmp:/tmp -v $XDG_RUNTIME_DIR/podman/podman.sock:/var/run/docker.sock example-services:latest
